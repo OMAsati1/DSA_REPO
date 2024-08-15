2a29ec381a6ccb1bbb0780bc1a790ca49da84e77
OMAsati1 <omasati@gmail.com>
2024-08-15 18:14:47 +0530

    k

---
+++ 4.cpp
@@ -0,0 +1,447 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     int i=1;
+//     cin >> n;
+//     while (i <= n)
+//     {
+//     int j=1;
+//         while (j <= n)
+//         {
+//             cout << n-j+1;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+
+//     return 0;
+// }
+// ...........................................................................................
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     int i=1;
+//     int count=1;
+//     cin >> n;
+//     while (i <= n)
+//     {
+//     int j=1;
+//         while (j <= n)
+//         {
+//             cout << count<<" ";
+//             count++;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     int i = 1;
+
+//     cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= i)
+//         {
+//             cout << "*" << " ";
+
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     int i = 1;
+//     int k = 1;
+
+//     cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= i)
+//         {
+//             cout << k << " ";
+//             k++;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n=5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         // int k = i;
+//         while (j <= i)
+//         {
+//             cout << j+i-1<< " ";
+
+//             // k++;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n=5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         // int k = i;
+//         while (j <= i)
+//         {
+//             cout << i-j+1<< " ";
+
+//             // k++;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n=5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         // int k = i;
+//         while (j <= n)
+//         {
+//             char ch = 'A'+j-1;
+//             cout << ch<< " ";
+
+//             // k++;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n=5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         // int k = i;
+//         while (j <= n)
+//         {
+//             char ch = 'A'+j+i-2;
+//             cout << ch<< " ";
+
+//             // k++;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n=5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         // int k = i;
+//         while (j <= i)
+//         {
+//             char ch = 'A'+i-1;
+//             cout << ch<< " ";
+
+//             // k++;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 5;
+//     int i = 1;
+//     char ch = 'A';
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= i)
+//         {
+//             cout << ch << " ";
+
+//             ch = ch + 1;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= i)
+//         {
+//     char ch = 'A'+i+j-2;
+//             cout << ch << " ";
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 4;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= i)
+//         {
+//     char ch = 'A'+n-i+j-1;
+//             cout << ch << " ";
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//             // int spc=n-i;
+//             int k =1;
+//         while (k<=n-i)
+//         {
+//             cout << " ";
+//             // spc--;
+//             k++;
+//         }
+//     int j = 1;
+//         while (j <= i)
+//         {
+//             cout << "*";
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//             // int spc=n-i;
+//             int k =1;
+//         while (k<=i)
+//         {
+//             cout << " ";
+//             // spc--;
+//             k++;
+//         }
+//     int j = 1;
+//         while (j <= n-i+1)
+//         {
+//             cout << "*";
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//             // int spc=n-i;
+//             int k =1;
+//         while (k<=i)
+//         {
+//             cout << " ";
+//             // spc--;
+//             k++;
+//         }
+//     int j = 1;
+//         while (j <= n-i+1)
+//         {
+//             cout << i;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n = 5;
+//     int i = 1;
+//     // cin >> n;
+//     while (i <= n)
+//     {
+//             // int spc=n-i;
+//             int k =1;
+//         while (k<=n-i)
+//         {
+//             cout << " ";
+//             // spc--;
+//             k++;
+//         }
+//     int j = 1;
+//         while (j <= i)
+//         {
+//             cout << j;
+//             j++;
+//         }
+//     int l = 1;
+//         while (l <= i-1)
+//         {
+//             cout << l;
+//             l++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+// // ...........................................................................................
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    int i = 1;
+    // cin >> n;
+    while (i <= n)
+    {
+            // int spc=n-i;
+            int k =1;
+        while (k<=n-i+1)
+        {
+            cout <<k;
+            // spc--;
+            k++;
+        }
+    int j = 1;
+        while (j <= 2*i-2)
+        {
+            cout << "*";
+            j++;
+        }
+    int l = 1;
+        while (l <= n-i+1)
+        {
+            cout << n-i-l+2;
+            l++;
+        }
+        cout << endl; 
+        i++;
+    }
+    return 0;
+}
+// ...........................................................................................
